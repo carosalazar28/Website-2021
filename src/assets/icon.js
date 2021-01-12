dccf65f0737d91d321e84518113d9e5dfbdf8843
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-01-11 20:03:59 -0500

    created the file for icons

---
+++ src/assets/icon.js
@@ -0,0 +1,8 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { 
+  faEnvelopeOpenText,
+  faBars,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faGithub, faLinkedin, faEnvelopeOpenText, faBars, );
