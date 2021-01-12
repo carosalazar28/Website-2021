5c37fc4e628bd8becc969085e7a5aa358126a380
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-01-11 20:04:40 -0500

    created page home

---
+++ src/pages/Home.js
@@ -0,0 +1,45 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  Name,
+  Carrer,
+  ContactInfo,
+  ParragrahpContact,
+  SectionQuoute,
+  IconMargin,
+  ParragrahpQuote,
+} from '../components/HomeStyled';
+import { Section } from '../components/MainStyled';
+
+
+function Home() {
+  return(
+    <Section>
+      <Name> Carolina <br/> Salazar </Name>
+      <Carrer> Front End Developer </Carrer>
+      <ContactInfo> 
+        <IconMargin icon={['fab', 'github']} size="2x"/>
+        <FontAwesomeIcon icon={['fab', 'linkedin']} size="2x"/>
+      </ContactInfo>
+      <ContactInfo>
+        <IconMargin icon={'envelope-open-text'} size="2x"/>
+        <ParragrahpContact>Contact me</ParragrahpContact>
+      </ContactInfo>
+      <SectionQuoute>
+        <figure>
+          <img 
+            src="https://res.cloudinary.com/dkcbxnhg0/image/upload/v1610402784/portfolio/cover_pasyhb.svg" 
+            alt="Imagen de cover para el home" 
+          />
+        </figure>
+        <blockquote>
+          <ParragrahpQuote>
+            "If you can't love yourself, how in the hell you gonna love somebody else"
+          </ParragrahpQuote>
+          <h4>RuPaul</h4>
+        </blockquote>
+      </SectionQuoute>
+    </Section>
+  )
+}
+
+export default Home
