64e0fd612cb948f853a39401725fa214ca56a4ab
Carolina Salazar <cindy.s.r.28@hotmail.com>
2021-02-25 16:06:22 -0500

    create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export function Footer() {
+  return (
+    <footer>
+      <div>
+        <p>Made with love by Carolina Salazar</p>
+        <p>© 2020 PORTFOLIO CAROLINA SALAZAR, All Rights Reserved</p>
+      </div>
+      <div>
+        <p>Currently living in Colombia, Bogotá D.C.</p>
+      </div>
+      <div>
+        <p><strong>Email:</strong> cindy.s.r.28@hotmail.com</p>
+        <p><strong>Phone: +</strong>3102139570</p>
+      </div>
+    </footer>
+  );
+}
